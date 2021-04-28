50a2e37cbcce42f19f58c752d026f7b2f4ac7270
Muhammet Safa Güner <muhammet.guner@hepsiburada.com>
2021-04-28 12:44:35 +0300

    clear

---
+++ src/tests/yedek.ts
@@ -0,0 +1,28 @@
+// import {chromium, Browser, Page} from "playwright";
+
+// let browser: Browser;
+// beforeAll(async () => {
+//     browser = await chromium.launch();
+// });
+// afterAll(async () => {
+//     await browser.close();
+// });
+//
+// let page: Page;
+// beforeEach(async () => {
+//     page = await browser.newPage();
+// });
+// afterEach(async () => {
+//     await page.close();
+// });
+
+
+describe("Hepsiburada test", () => {
+    it("search case", async () => {
+        await page.goto("http://www.hepsiburada.com/");
+        await page.type('input[class="desktopOldAutosuggestTheme-input"]', "Playwright");
+        await page.click('div[class="SearchBoxOld-buttonContainer"]');
+        await page.screenshot({path: `./screenshots/hb-${browserName}.png`});
+    })
+
+})
